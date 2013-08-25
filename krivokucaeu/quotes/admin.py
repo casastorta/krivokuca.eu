3669bb268393a620c6747c7c744091b2e35441d9
Vedran Krivokuca <vedran.krivokuca@nimium.hr>
2013-08-25 11:51:52 +0200

    ClickAdmin tweaking

--- krivokucaeu/quotes/admin.py
+++ krivokucaeu/quotes/admin.py
@@ -9,7 +9,13 @@ class QuoteAdmin(admin.ModelAdmin):
     date_hierarchy = 'date_added'
 
 
+class ClickAdmin(admin.ModelAdmin):
+    list_display = ('click_url', 'click_type', 'click_destination')
+    list_filter = ('click_type', 'click_destination', 'click_datetime')
+    date_hierarchy = 'click_datetime'
+
+
 admin.site.register(qmodels.Quote, QuoteAdmin)
 admin.site.register(qmodels.Book)
 admin.site.register(qmodels.Author)
-admin.site.register(qmodels.ClickLog)
+admin.site.register(qmodels.ClickLog, ClickAdmin)
