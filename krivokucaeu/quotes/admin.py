848c5c17988c9d1ef711dc20f977025f84ea4af0
Vedran Krivokuca <vedran.krivokuca@nimium.hr>
2013-08-25 11:46:48 +0200

    Click logs added to db.

--- krivokucaeu/quotes/admin.py
+++ krivokucaeu/quotes/admin.py
@@ -5,9 +5,11 @@ from quotes import models as qmodels
 class QuoteAdmin(admin.ModelAdmin):
     quote_display = lambda m, q: '%s' % (q)
     list_display = ('quote_display', 'book', 'date_added', )
-    list_filter = ('book',)
+    list_filter = ('book', 'date_added')
+    date_hierarchy = 'date_added'
 
 
 admin.site.register(qmodels.Quote, QuoteAdmin)
 admin.site.register(qmodels.Book)
 admin.site.register(qmodels.Author)
+admin.site.register(qmodels.ClickLog)
--- krivokucaeu/quotes/models.py
+++ krivokucaeu/quotes/models.py
@@ -7,10 +7,10 @@ class Book(models.Model):
     '''
     title = models.CharField(max_length=256)
     extended_title = models.TextField(blank=True)
-    amazon_url = models.TextField(max_length=2000, blank=True)
+    amazon_url = models.URLField(blank=True)
     amazon_is_referal = models.BooleanField(default=True, blank=True)
-    wikipedia_url = models.TextField(max_length=2000, blank=True)
-    goodreads_url = models.TextField(max_length=2000, blank=True)
+    wikipedia_url = models.URLField(blank=True)
+    goodreads_url = models.URLField(blank=True)
     short_description = models.TextField(blank=True)
 
     def __unicode__(self):
@@ -23,10 +23,10 @@ class Author(models.Model):
     '''
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
-    amazon_url = models.TextField(max_length=2000, blank=True)
+    amazon_url = models.URLField(blank=True)
     amazon_is_referal = models.BooleanField(default=True, blank=True)
-    wikipedia_url = models.TextField(max_length=2000, blank=True)
-    goodreads_url = models.TextField(max_length=2000, blank=True)
+    wikipedia_url = models.URLField(blank=True)
+    goodreads_url = models.URLField(blank=True)
     short_bio = models.TextField(blank=True)
     # Relation towards books
     books = models.ManyToManyField(Book, blank=True)
@@ -36,12 +36,38 @@ class Author(models.Model):
 
 
 class Quote(models.Model):
+    '''
+    Quotes table db model
+    '''
     quote_text = models.TextField()
     quote_context = models.TextField(blank=True)
-    book = models.ForeignKey(Book)
     date_added = models.DateField()
+    # Relation towards books
+    book = models.ForeignKey(Book)
 
     def __unicode__(self):
         # On the fly replace potential newlines not to screw up
         # django admin interface.
         return u'%s\u2026' % (self.quote_text[0:128].replace('\n', ' '))
+
+
+class ClickLog(models.Model):
+    '''
+    Outgoing clicks tracking db table model
+    '''
+    CLICK_TYPES = (
+        ('A', 'Author'),
+        ('B', 'Book'),
+    )
+    CLICK_DESTINATIONS = (
+        ('GR', 'GoodReads'),
+        ('AMZN', 'Amazon'),
+        ('WIKI', 'Wikipedia'),
+    )
+    click_url = models.URLField()
+    click_type = models.CharField(max_length=4, choices=CLICK_TYPES)
+    click_destination = \
+        models.CharField(max_length=4, choices=CLICK_DESTINATIONS)
+    click_type_destination_id = models.BigIntegerField()
+    click_datetime = models.DateTimeField()
+    click_user_address = models.GenericIPAddressField()
