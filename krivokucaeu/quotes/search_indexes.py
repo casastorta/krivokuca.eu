cc62a8ca65c3b5bfd59a33568e985dd5a3011309
Vedran Krivokuca <vedran.krivokuca@nimium.hr>
2013-08-30 09:25:02 +0200

    Search layout minor fixes.

--- krivokucaeu/quotes/search_indexes.py
+++ krivokucaeu/quotes/search_indexes.py
@@ -6,8 +6,6 @@ from quotes.models import Author, Book, Quote
 class BookIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
-    #extended_title = indexes.CharField(model_attr='extended_title')
-    #short_description = indexes.CharField(model_attr='short_description')
     object_type = indexes.CharField(model_attr='object_type')
     pub_date = indexes.DateTimeField(model_attr='date_added')
 
@@ -25,7 +23,6 @@ class AuthorIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     first_name = indexes.CharField(model_attr='first_name')
     last_name = indexes.CharField(model_attr='last_name')
-    #short_bio = indexes.CharField(model_attr='short_bio')
     object_type = indexes.CharField(model_attr='object_type')
     pub_date = indexes.DateTimeField(model_attr='date_added')
 
@@ -41,8 +38,6 @@ class AuthorIndex(indexes.SearchIndex, indexes.Indexable):
 
 class QuoteIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    #quote_text = indexes.CharField(model_attr='quote_text')
-    #quote_context = indexes.CharField(model_attr='quote_context')
     object_type = indexes.CharField(model_attr='object_type')
     book = indexes.CharField(model_attr='book')
     pub_date = indexes.DateTimeField(model_attr='date_added')
